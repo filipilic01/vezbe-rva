cbaadf56beeea16481a7c73952b197ab07b0b002
filip <filip@filipIlic>
2023-03-06 01:26:43 +0100

    initial project

---
+++ vezbeProj/src/test/java/RVA/VezbeProjApplicationTests.java
@@ -0,0 +1,13 @@
+package RVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VezbeProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
